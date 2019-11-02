4479cd1cf7868eb4625cd9d35ec7e9d9c43e896d
alexbhdez <abhbretones@gmail.com>
2019-11-02 19:48:50 +0100

    update email by username in user model

--- routes/auth.js
+++ routes/auth.js
@@ -11,16 +11,16 @@ const { isLogged, isNotLogged } = require('../middlewares/auth');
 router.get('/me', isLogged(), (req, res) => res.status(200).json(req.session.currentUser));
 
 router.post('/signup', isNotLogged(), async (req, res, next) => {
-  const { email, password } = req.body;
-  (!email || !password) && next(createError(422, 'Fields are empty'));
+  const { username, password } = req.body;
+  (!username || !password) && next(createError(422, 'Fields are empty'));
 
   try {
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ username });
     user && next(createError(422, 'User aldready exists'));
 
     const salt = bcrypt.genSaltSync(parseInt(process.env.BCRYPT));
     const hashPass = bcrypt.hashSync(password, salt);
-    const newUser = await User.create({ email, password: hashPass });
+    const newUser = await User.create({ username, password: hashPass });
 
     req.session.currentUser = newUser;
     res.status(200).json(newUser);
@@ -30,25 +30,29 @@ router.post('/signup', isNotLogged(), async (req, res, next) => {
 });
 
 router.post('/login', isNotLogged(), async (req, res, next) => {
-  const { email, password } = req.body;
+  const { username, password } = req.body;
 
   try {
-    const user = await User.findOne({ email });
-    !user && next(createError(422, 'User already exists'));
+    const user = await User.findOne({ username });
 
-    if (bcrypt.compare(password, user.password)) {
-      req.session.currentUser = user;
-      return res.status(200).json(user);
+    if (user) {
+      if (bcrypt.compare(password, user.password)) {
+        req.session.currentUser = user;
+        return res.status(200).json(user);
+      } else {
+        next(createError(401, 'Wrong user or password'));
+      }
+    } else {
+      next(createError(422, 'You dont have an account, please signup'));
     }
-    next(createError(401, 'Wrong user or password'));
   } catch (error) {
     next(error);
   }
 });
 
-router.post('/logout', (req, res) => {
+router.get('/logout', (req, res) => {
   req.session.destroy();
-  return res.status(204);
+  return res.status(204).json({ loggedOut: true });
 });
 
 module.exports = router;
